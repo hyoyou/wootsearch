4cfb5cb58086eff4b4195161c385484c310bbc98
Purnima Lakshminarayanan <purnima.nyc@gmail.com>
2024-03-19 15:05:08 -0400

    added display items view

--- wootsearch_api/myapi/views.py
+++ wootsearch_api/myapi/views.py
@@ -3,5 +3,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Hello, world!'})
+def display_items(request):
+    # load env file with API key
+    # https://stackoverflow.com/questions/40216311/reading-in-environment-variables-from-an-environment-file
+    # make the api call
+    # manipulate data and return to client
+    return Response({'message': 'This is a list of items'})
